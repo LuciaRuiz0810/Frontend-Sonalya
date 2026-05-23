b9a8eecc1965c9e2106e45b27c5d8298ce45eec9
Lucia <adri.morgon1109@gmail.com>
2026-05-23 14:48:52 +0200

    url arreglada

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,4 +1,4 @@
-const URL_API = 'http://localhost:8000/api'
+const URL_API = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 function obtenerCabeceras(esFormData = false) {
     const token = localStorage.getItem('token_auth')
